5528e1386b6fc0a66f7457ead1cc166f0130b900
KoalaTea <koalateac@gmail.com>
2015-06-23 21:21:20 -0400

    Added scheduler so it will backup every hour

--- gigasetlogs.py
+++ gigasetlogs.py
@@ -21,48 +21,71 @@ while (len(result)>0):
 """
 import sys,urllib.request,re
 import os.path
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+#set up the scheduler to run every hour
+scheduler = BlockingScheduler()
 
 #setting up and grabbing the data
 J="http://192.168.254.254/syslogshow.htm"
 A=urllib.request.urlopen(J)
 AB=A.read()
 
-#check if GigasetBackup exists
-if os.path.exists('GigasetBackup.txt'):
-    #read for the last line of the file
-    file = open('GigasetBackup.txt', 'r')
-    for line in file:
-        lastentry = line
-    file.close()
+'''
+Backup()
+the main logic of the program
 
-    #open backup and add to the end of backupfile any new entries
-    file = open('GigasetBackup.txt', 'a')
-    data=str(AB).split('<TR>')
-    new=0;
-    for line in data:
-        entry = re.search(r'<pre>(.*)</pre>',line,flags=0)
-        if entry and new == 1:
-            file.write(entry.group(1) + '\n')
-        elif entry:
-            newentry = re.split('/|:|\s',entry.group(1))
-            lastentrysp = re.split('/|:|\s', lastentry)
-            
-            #checking dates for newer entry by concat yearmonthdayhourminsec
-            #and then comparing
-            newentryj = newentry[2] + newentry[0] + newentry[1] + newentry[3]
-            newentryj = newentryj + newentry[4] + newentry[5]
-            lastentryj = lastentrysp[2] + lastentrysp[0] + lastentrysp[1]
-            lastentryj = lastentryj + lastentrysp[3] + lastentrysp[4]
-            lastentryj = lastentryj + lastentrysp[5]
-            if int(newentryj) > int(lastentryj):
-                new = 1
+Creates a Backup file of the logs if a backup file does not exist.
+if a file does exist itchecks if the logs have any new entries
+and if it does it appends them to the end of the file
+'''
+def Backup():
+    #check if GigasetBackup exists
+    if os.path.exists('GigasetBackup.txt'):
+        #read for the last line of the file
+        file = open('GigasetBackup.txt', 'r')
+        for line in file:
+            lastentry = line
+        file.close()
+
+        #open backup and add to the end of backupfile any new entries
+        file = open('GigasetBackup.txt', 'a')
+        data=str(AB).split('<TR>')
+        new=0;
+        for line in data:
+            entry = re.search(r'<pre>(.*)</pre>',line,flags=0)
+            if entry and new == 1:
                 file.write(entry.group(1) + '\n')
-else:
-    file = open('GigasetBackup.txt', 'w')
-    data=str(AB).split('<TR>')
-    new=0;
-    for line in data:
-        entry = re.search(r'<pre>(.*)</pre>',line,flags=0)
-        if entry:
-            file.write(entry.group(1) + '\n')
-file.close()
+            elif entry:
+                newentry = re.split('/|:|\s',entry.group(1))
+                lastentrysp = re.split('/|:|\s', lastentry)
+            
+                #checking dates for newer entry by concat yearmonthdayhourminsec
+                #and then comparing
+                newentryj = newentry[2] + newentry[0] + newentry[1] + newentry[3]
+                newentryj = newentryj + newentry[4] + newentry[5]
+                lastentryj = lastentrysp[2] + lastentrysp[0] + lastentrysp[1]
+                lastentryj = lastentryj + lastentrysp[3] + lastentrysp[4]
+                lastentryj = lastentryj + lastentrysp[5]
+                if int(newentryj) > int(lastentryj):
+                    new = 1
+                    file.write(entry.group(1) + '\n')
+    else:
+        file = open('GigasetBackup.txt', 'w')
+        data=str(AB).split('<TR>')
+        new=0;
+        for line in data:
+            entry = re.search(r'<pre>(.*)</pre>',line,flags=0)
+            if entry:
+                file.write(entry.group(1) + '\n')
+    file.close()
+    scheduler.add_job(Backup, 'interval', hours=1)
+
+
+scheduler.add_job(Backup, 'interval', hours=1)
+print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+try:
+    scheduler.start()
+except (KeyboardInterrupt, SystemExit):
+    raise
